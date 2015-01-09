27c5871920d5833167cf6b330e0c4182956669d1
Akito Gyoten <akitogyoten@gmail.com>
2015-01-09 18:18:38 +0900

    change "install" command for mbed-package

--- lib/iot/install.rb
+++ lib/iot/install.rb
@@ -7,32 +7,43 @@ ArmGccLink = "https://launchpad.net/gcc-arm-embedded/4.9/4.9-2014-q4-major/+down
 ArmGccTarPath = File.expand_path "#{StroboPath}/#{ File.basename ArmGccLink }"
 ArmGccPath = "#{StroboPath}/gcc-arm-none-eabi-4_9-2014q4"
 
-NrfSdkLink = "http://developer.nordicsemi.com/nRF51_SDK/nRF51_SDK_7.1.0_372d17a.zip"
-NrfSdkZipPath = File.expand_path "#{StroboPath}/#{File.basename NrfSdkLink}"
-NrfSdkPath = "#{StroboPath}/nrf51_sdk"
-
-SoftDeviceLink = ""
+Mbed_Dev_Url = "http://developer.mbed.org/"
+Mbed_Ble_Api_Link = "#{Mbed_Dev_Url}teams/Bluetooth-Low-Energy/code/BLE_API/archive/fb2a891a0d98.zip"
+Mbed_Nrf51822_Link = "#{Mbed_Dev_Url}teams/Nordic-Semiconductor/code/nRF51822/archive/17fe69405098.zip"
+Mbed_Mbed_Link = "#{Mbed_Dev_Url}users/mbed_official/code/mbed/archive/4fc01daae5a5.zip"
 
 
 # Install packages for offline compile
 module Iot
   module Install
-    
-    def installComponents
-      makeStroboDir
-      getArmGcc
-      getNrf51SDK
-    end
 
     def uninstall
       executeOut, _, _ = *Open3.capture3("rm -rf #{StroboPath}")
       puts "rm -rf #{StroboPath}/*"
     end
-    
+
+    def installComponents
+      make_strobo_dir
+
+      # Install mbed Libraries:
+      # mbed, BLE_API, nrf51822
+      zip_links = [
+        Mbed_Ble_Api_Link,
+        Mbed_Nrf51822_Link,
+        Mbed_Mbed_Link
+      ]
+      zip_links.each do |zip_link|
+        get_lib_zip zip_link
+      end
+
+      get_gcc_arm
+    end
+
+    private
+
     # Create strobo directory.
-    # Check existing of /usr/local/strobo
-    # before creating.
-    def makeStroboDir
+    # Check existing of /usr/local/strobo before creating
+    def make_strobo_dir
       if File.exist? StroboPath
         puts "already exist #{StroboPath}"
       else
@@ -43,7 +54,7 @@ module Iot
 
     # Install Arm-GCC
     # see here: https://launchpad.net/gcc-arm-embedded
-    def getArmGcc
+    def get_gcc_arm
       if File.exist? ArmGccPath
         puts "already exist #{ArmGccPath}"
         return
@@ -58,30 +69,25 @@ module Iot
       cmd = "tar zxvf #{ ArmGccTarPath } -C #{StroboPath}"
       puts cmd
       executeOut, _, _ = *Open3.capture3(cmd)
-      
+
       `rm #{ArmGccTarPath}`
     end
 
-    def getNrf51SDK
-
-      if !File.exist? NrfSdkPath
-        Dir.mkdir NrfSdkPath
-      else
-        puts "already exist #{NrfSdkPath}"
-        return
-      end
-      
-      cmd = "wget #{NrfSdkLink} -P #{StroboPath}"
+    def get_lib_zip lib_path
+      cmd = "wget #{lib_path} -P #{StroboPath}"
       puts cmd
       `#{cmd}`
 
-      cmd = "unzip #{NrfSdkZipPath} -d #{NrfSdkPath}"
-      puts cmd
-      executeOut, _, _ = *Open3.capture3(cmd)
-
-      `rm #{NrfSdkZipPath}`      
+      cmd = []
+      cmd << "unzip #{StroboPath}/*.zip -d #{StroboPath}"
+      cmd << "rm #{StroboPath}/*.zip"
+      cmd << "mv #{StroboPath}/nRF51822* #{StroboPath}/nRF51822"
+      cmd << "mv #{StroboPath}/mbed* #{StroboPath}/mbed"
+      cmd << "mv #{StroboPath}/BLE_API* #{StroboPath}/BLE_API"
+      cmd.each do |temp|
+        execute_out, _, _ = *Open3.capture3(temp)
+      end
     end
 
-    
   end
 end
